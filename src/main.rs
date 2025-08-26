17d176c99b3fbc491a1a52bbf028718dd445fc73
sadneo <haywardeel@gmail.com>
2025-08-26 19:09:35 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
-use std::fs::{self, OpenOptions};
+use std::fs::OpenOptions;
 use std::io::{Seek, SeekFrom};
 use std::path::PathBuf;
 use std::process::Command;
 use std::time::SystemTime;
+
+use tokio::fs;
 
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -16,15 +18,16 @@ struct EventObject {
     app_id: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let log_path = PathBuf::from(std::env::var("XDG_STATE_HOME").expect("$XDG_STATE_HOME invalid"))
         .join("focus-tracker.json");
     println!("{}", log_path.display());
 
-    match fs::exists(&log_path) {
+    match fs::try_exists(&log_path).await {
         Ok(true) => (),
         Ok(false) => {
-            if let Err(e) = fs::write(&log_path, b"[]") {
+            if let Err(e) = fs::write(&log_path, b"[]").await {
                 eprintln!("error: {}", e);
             }
         },
@@ -41,11 +44,11 @@ fn main() {
 
         let object: HashMap<String, Value> =
             serde_json::from_slice(&sub_result.stdout).expect("serde from_json failed");
-        add_log(object, &log_path).expect("add log failed");
+        add_log(object, &log_path).await.expect("add log failed");
     }
 }
 
-fn add_log(object: HashMap<String, Value>, path: &PathBuf) -> std::io::Result<()> {
+async fn add_log(object: HashMap<String, Value>, path: &PathBuf) -> std::io::Result<()> {
     let change_type = object.get("change").unwrap().as_str().unwrap().to_owned();
     if !matches!(change_type.as_str(), "new" | "close" | "focus") {
         return Ok(());
@@ -75,17 +78,19 @@ fn add_log(object: HashMap<String, Value>, path: &PathBuf) -> std::io::Result<()
         app_id,
     };
 
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(path)?;
+    // tokio here
+    tokio::task::spawn_blocking(move || {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(path)?;
 
-    let mut events: Vec<EventObject> = serde_json::from_reader(&file).unwrap_or_default();
-    events.push(new_event);
-    file.set_len(0)?;
-    file.seek(SeekFrom::Start(0))?;
-    serde_json::to_writer_pretty(&file, &events)?;
-
+        let mut events: Vec<EventObject> = serde_json::from_reader(&file).unwrap_or_default();
+        events.push(new_event);
+        file.set_len(0)?;
+        file.seek(SeekFrom::Start(0))?;
+        serde_json::to_writer_pretty(&file, &events)?;
+    }).await.unwrap();
     Ok(())
 }
