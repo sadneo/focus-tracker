50629e2708e3e7804069316fc35529dfffb24be1
sadneo <haywardeel@gmail.com>
2025-08-27 09:46:19 -0400

    fix: web server works now

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use tokio::fs::{self, OpenOptions};
 use tokio::io::{AsyncSeekExt, AsyncWriteExt};
 use tokio::sync::Mutex;
 
-use axum::{routing::get, Router, Json, extract::State};
+use axum::{Json, Router, extract::State, routing::get};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -27,12 +27,12 @@ type EventState = Arc<Mutex<Vec<EventObject>>>;
 async fn main() {
     let log_path = PathBuf::from(std::env::var("XDG_STATE_HOME").expect("$XDG_STATE_HOME invalid"))
         .join("focus-tracker.json");
-    println!("{}", log_path.display());
 
     let events: Vec<EventObject> = match fs::try_exists(&log_path).await {
         Ok(true) => {
             let file_contents = fs::read(&log_path).await.unwrap();
-            let events: Vec<EventObject> = serde_json::from_slice(&file_contents).unwrap_or_default();
+            let events: Vec<EventObject> =
+                serde_json::from_slice(&file_contents).unwrap_or_default();
             events
         }
         Ok(false) => {
@@ -45,22 +45,26 @@ async fn main() {
         Err(e) => {
             eprintln!("error: {}", e);
             return;
-        },
+        }
     };
     let events = Arc::new(Mutex::new(events));
 
     let app: Router<()> = Router::new()
-        .route("/api/data", get(|State(state): State<EventState> | async move {
-            let events_guard = state.lock().await;
-            let ser_contents = serde_json::to_string_pretty(&*events_guard).unwrap();
-            Json(ser_contents)
-        }))
+        .route(
+            "/api/data",
+            get(|State(state): State<EventState>| async move {
+                let events_guard = state.lock().await;
+                let ser_contents = serde_json::to_string_pretty(&*events_guard).unwrap();
+                Json(ser_contents)
+            }),
+        )
+        .fallback_service(tower_http::services::ServeDir::new("frontend/dist"))
         .with_state(events.clone());
 
-    let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
-    tokio::task::spawn(async move {
-        axum::serve(listener, app).await
-    });
+    let listener = tokio::net::TcpListener::bind("localhost:3000")
+        .await
+        .unwrap();
+    tokio::task::spawn(async move { axum::serve(listener, app).await });
 
     loop {
         let sub_result = Command::new("swaymsg")
@@ -72,11 +76,17 @@ async fn main() {
 
         let object: HashMap<String, Value> =
             serde_json::from_slice(&sub_result.stdout).expect("serde from_json failed");
-        add_log(object, &log_path, events.clone()).await.expect("add log failed");
+        add_log(object, &log_path, events.clone())
+            .await
+            .expect("add log failed");
     }
 }
 
-async fn add_log(object: HashMap<String, Value>, path: &PathBuf, events: EventState) -> std::io::Result<()> {
+async fn add_log(
+    object: HashMap<String, Value>,
+    path: &PathBuf,
+    events: EventState,
+) -> std::io::Result<()> {
     let change_type = object.get("change").unwrap().as_str().unwrap().to_owned();
     if !matches!(change_type.as_str(), "new" | "close" | "focus") {
         return Ok(());
