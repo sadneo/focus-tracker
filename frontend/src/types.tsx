0e8ea45a0b2d9968c80e9c8dbcbbd9fa9a6a58a5
sadneo <haywardeel@gmail.com>
2025-09-06 10:54:36 -0400

    fix: incorrect types

--- frontend/src/types.tsx
+++ frontend/src/types.tsx
@@ -1,6 +1,9 @@
 export interface EventObject {
   change_type: string;
-  timestamp: string;
+  timestamp: {
+    nanos_since_epoch: number;
+    secs_since_epoch: number;
+  };
   id: number;
   app_id: string;
 }
